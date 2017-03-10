d7a835e25b69b0830c491cbdcd73120ccb28595c
Yicheng Zhang <zhangyicheng1234@gmail.com>
2017-03-10 17:48:58 -0500

    Initial commit of fork

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,16 @@
 'use strict';
-
+navigator.getUserMedia = navigator.getUserMedia ||
+navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+var constraints = {
+  audio: false,
+  video: true
+};
+var localVideo = document.getElementById("localVideo")
+// var localVideo = document.querySelector('video');
+navigator.getUserMedia(constraints,
+  function(stream){
+    window.stream = stream;
+    localVideo.srcObject = stream;
+  }, function(error){
+    console.log("getUserMedia error: " + error.name)
+  });
